76c18990b81009f090a1080a9c8a49c5784bdba4
tmdgus7983 <tmdgus7983@gmail.com>
2024-12-10 15:36:19 +0900

    [feat]메인화면 javascript 카테고리 전체조회 비동기 요청, [fix] 메인화면 mustache 및 css 수정

--- src/main/java/com/metacoding/web_project/goods/GoodsController.java
+++ src/main/java/com/metacoding/web_project/goods/GoodsController.java
@@ -4,15 +4,20 @@ import jakarta.servlet.http.HttpSession;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+<<<<<<< HEAD
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+=======
+import org.springframework.web.bind.annotation.GetMapping;
+>>>>>>> d56edf2 ([feat]git메인화면 javascript 카테고리 전체조회 비동기 요청, [fix] 메인화면 mustache 및 css 수정)
 
 @RequiredArgsConstructor
 @Controller
 public class GoodsController {
     private final GoodsService goodsService;
 
+<<<<<<< HEAD
     private final HttpSession session;
 
     // 제품 상세페이지 불러오기
@@ -34,4 +39,11 @@ public class GoodsController {
     public String register(Model model) {
         return "goods-register";
     }
+=======
+    @GetMapping("/goods-list")
+    public String goodsList(){
+        return "goods-list";
+    }
+
+>>>>>>> d56edf2 ([feat]git메인화면 javascript 카테고리 전체조회 비동기 요청, [fix] 메인화면 mustache 및 css 수정)
 }
--- src/test/java/com/metacoding/web_project/category/CategoryRepositoryTest.java
+++ src/test/java/com/metacoding/web_project/category/CategoryRepositoryTest.java
@@ -16,8 +16,18 @@ public class CategoryRepositoryTest {
     private CategoryRepository categoryRepository;
 
     @Test
+<<<<<<< HEAD
     public void findAllCategory_test() {
         List<Category> list = categoryRepository.findAllCategory();
         System.out.println(list.size());
+=======
+    public void findAll_test(){
+        List<Category> category = categoryRepository.findAll();
+
+        for(Category c : category){
+            System.out.println(c.getName());
+        }
+
+>>>>>>> d56edf2 ([feat]git메인화면 javascript 카테고리 전체조회 비동기 요청, [fix] 메인화면 mustache 및 css 수정)
     }
 }
