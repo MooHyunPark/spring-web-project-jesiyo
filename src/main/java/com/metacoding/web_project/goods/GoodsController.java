db4434a64263ef9b98b7b8089a1ee4195bc8cd2a
moohyun <angus1208@naver.com>
2024-12-15 22:39:11 +0900

    [feat] 마이페이지 관련 모든 페이징 처리 완료 최대 출력 개수는 한페이지당 3개

--- src/main/java/com/metacoding/web_project/goods/GoodsController.java
+++ src/main/java/com/metacoding/web_project/goods/GoodsController.java
@@ -1,9 +1,13 @@
 package com.metacoding.web_project.goods;
 
 import com.metacoding.web_project._core.CommonResp;
+import com.metacoding.web_project._core.util.PageUtil;
 import com.metacoding.web_project.bid.BidResponse;
+import com.metacoding.web_project.category.CategoryResponse;
 import com.metacoding.web_project.category.CategoryService;
+import com.metacoding.web_project.user.User;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +15,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,10 +33,6 @@ public class GoodsController {
     @GetMapping("/goods-detail/{id}")
     public String goodsDetail(@PathVariable("id")Integer id, Model model) {
 
-        // ***임시*** // 세션에 username 저장
-        String username = "ssar";
-        session.setAttribute("username", username);
-
         GoodsResponse.GoodsDetailDTO goodsDetailDTO = goodsService.getGoodsInfo(id);
         model.addAttribute("goods", goodsDetailDTO);
 
@@ -40,15 +41,18 @@ public class GoodsController {
 
     // 제품 등록 화면 열기
     @GetMapping("/s/myPage-goods-register")
-    public String register() {
+    public String register(@AuthenticationPrincipal User user, Model model) {
+        List<CategoryResponse.CategoryDTO> allCategory = categoryService.findAllCategory(null);
+        model.addAttribute("model", allCategory);
+        model.addAttribute("sellerId", user.getId());
         return "goods-register";
     }
 
     // 제품 등록
-    @PostMapping("/goods/save")
-    public String goodsSave(GoodsRequest.GoodsSaveDTO goodsSaveDTO) {
+    @PostMapping("/s/goods/save")
+    public String goodsSave(@Valid GoodsRequest.GoodsSaveDTO goodsSaveDTO, Errors errors) {
         goodsService.goodsSave(goodsSaveDTO);
-        return "redirect:/mypage-being-auctioned"; // 경매중인물품 리스트 화면으로 리다이렉트
+        return "redirect:/s/myPage-being-auctioned"; // 경매중인물품 리스트 화면으로 리다이렉트
     }
 
     // 경매시간 종료 상품 상태 변경
@@ -63,16 +67,17 @@ public class GoodsController {
     }
 
     @GetMapping("/goods-list")
-    public String goodsList(@RequestParam("select") String select,@RequestParam("keyword") String keyword, Model model) {
-        model.addAttribute("category", categoryService.findAllCategory());
+    public String goodsList(@RequestParam("select") String select,@RequestParam(defaultValue = "") String keyword, Model model) {
+        model.addAttribute("category", categoryService.findAllCategory(null));
         model.addAttribute("goods", goodsService.searchGoodsList(GoodsRequest.SeacherGoodsDTO.builder().select(select).keyword(keyword).page(1).line(15).build()));
+        model.addAttribute("keyword", new GoodsResponse.KeyWordDTO(keyword));
         return "goods-list";
     }
 
     @GetMapping("/goods-list/{id}")
-    public String goodsList(@PathVariable("id") Integer id, Model model) {
-        model.addAttribute("category", categoryService.findAllCategory());
-        model.addAttribute("goods",goodsService.getGoodsList(id,1,15));
+    public String goodsList(@PathVariable("id") Integer categoryId, Model model) {
+        model.addAttribute("category", categoryService.findAllCategory(categoryId));
+        model.addAttribute("goods",goodsService.getGoodsList(categoryId,1,15));
         return "goods-list";
     }
 
@@ -93,11 +98,12 @@ public class GoodsController {
     }
 
     // 경매 중인 물품(판매) 화면 열기
-    @GetMapping("/mypage-being-auctioned")
-    public String beingAuctioned(@AuthenticationPrincipal UserDetails userDetails, Model model) {
-        model.addAttribute("models", goodsService.mySellGoods(userDetails.getUsername()));
+    @GetMapping("/s/myPage-being-auctioned")
+    public String beingAuctioned(@AuthenticationPrincipal User user, Model model, @RequestParam(defaultValue = "") String page) {
+        List<GoodsResponse.UserGoodsDTO> userGoodsDTOS = goodsService.mySellGoods(user.getUsername(), page);
+        int rowCount = goodsService.mySellGoodsAllCount(user.getId());
+        model.addAttribute("pagination", PageUtil.returnToPageDTO(page, rowCount, 3));
+        model.addAttribute("models", userGoodsDTOS);
         return "being-auctioned";
     }
-
-
 }
