d289ef3edb8dd2b63b8ecb5ee80a7b53221f1976
SeeunJoe <whtpdms1556@naver.com>
2024-12-15 22:53:52 +0900

    [fix] error수정

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,16 +1,16 @@
 
 -- user_tb 더미데이터
-insert into user_tb(username, password, name, post_num, addr, addr_detail, birth, role, tel) values('ssar', '$2a$10$vUQrTY7IN0lGJdlv/E.6n.I09krJTYX3tQsRiSZ4S6PDCQws9Kq5m', 'Ssar', '12345', '주소1', '상세주소1', '생일1', 'ROLE_ADMIN,ROLE_USER', '01011111111');
+insert into user_tb(username, password, name, post_num, addr, addr_detail, birth, role, tel) values('ssar', '$2a$10$vUQrTY7IN0lGJdlv/E.6n.I09krJTYX3tQsRiSZ4S6PDCQws9Kq5m', 'Ssar', '12345', '주소1', '상세주소1', '생일1', 'ROLE_ADMIN', '01011111111');
 insert into user_tb(username, password, name, post_num, addr, addr_detail, birth, role, tel) values('cos', '$2a$10$vUQrTY7IN0lGJdlv/E.6n.I09krJTYX3tQsRiSZ4S6PDCQws9Kq5m', 'Cos', '12345', '주소2', '상세주소1', '생일2', 'ROLE_USER', '01022222222');
 insert into user_tb(username, password, name, post_num, addr, addr_detail, birth, role, tel) values('love', '$2a$10$vUQrTY7IN0lGJdlv/E.6n.I09krJTYX3tQsRiSZ4S6PDCQws9Kq5m', 'Love', '12345', '주소2', '상세주소1', '생일2', 'ROLE_USER', '01033333333');
 insert into user_tb(username, password, name, post_num, addr, addr_detail, birth, role, tel) values('cos2', '$2a$10$vUQrTY7IN0lGJdlv/E.6n.I09krJTYX3tQsRiSZ4S6PDCQws9Kq5m', 'Cos2', '12345', '주소2', '상세주소1', '생일2', 'ROLE_USER', '01022222222');
 insert into user_tb(username, password, name, post_num, addr, addr_detail, birth, role, tel) values('love2', '$2a$10$vUQrTY7IN0lGJdlv/E.6n.I09krJTYX3tQsRiSZ4S6PDCQws9Kq5m', 'Love2', '12345', '주소2', '상세주소1', '생일2', 'ROLE_USER', '01033333333');
 
-insert into useraccount_tb(user_id, score, has_price) values (1, 0, 0);
-insert into useraccount_tb(user_id, score, has_price) values (2, 0, 3000);
-insert into useraccount_tb(user_id, score, has_price) values (3, 0, 3000);
-insert into useraccount_tb(user_id, score, has_price) values (4, 0, 3000);
-insert into useraccount_tb(user_id, score, has_price) values (5, 0, 3000);
+insert into useraccount_tb(user_id, score, has_price, account) values (1, 0, 0,'등록계좌없음');
+insert into useraccount_tb(user_id, score, has_price, account) values (2, 0, 15000,'등록계좌없음');
+insert into useraccount_tb(user_id, score, has_price, account) values (3, 0, 15000,'등록계좌없음');
+insert into useraccount_tb(user_id, score, has_price, account) values (4, 0, 15000,'등록계좌없음');
+insert into useraccount_tb(user_id, score, has_price, account) values (5, 0, 15000,'등록계좌없음');
 
 
 
@@ -27,10 +27,10 @@ insert into category_tb(name, img_url) values ('남성패션', '/upload/category
 
 -- goods_tb 더미데이터
 
-insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('강아지 산책줄 나눔합니다.', '4', '2', '강아지 산책줄 나눔 중입니다.', '없습니다.', 1000, '2024-12-10 12:20:00','2024-12-13 16:15:00', 0);
+insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('강아지 산책줄 나눔합니다.', '4', '2', '강아지 산책줄 나눔 중입니다.', '/upload/category1.png', 1000, '2024-12-10 12:20:00','2024-12-13 16:15:00', 0);
 insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('책상 팝니다', '7', '1', '2년 썼고 생활기스 있어요.', '없습니다', 20000, now(),'2024-12-12 22:00:00', 0);
 
-insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('강아지 산책줄 나눔합니다.', '4', '2', '강아지 산책줄 나눔 중입니다.', '없습니다.', 1000, '2024-12-10 12:20:00','2024-12-13 12:55:00', 0);
+insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('강아지 산책줄 나눔합니다.', '4', '2', '강아지 산책줄 나눔 중입니다.', '/upload/category1.png', 1000, '2024-12-10 12:20:00','2024-12-13 12:55:00', 0);
 insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('책상 팝니다', '7', '3', '2년 썼고 생활기스 있어요.', '없습니다', 20000, now(),'2024-12-12 22:00:00', 0);
 
 
@@ -45,7 +45,7 @@ insert into bid_tb(buyer_id,goods_id,try_price,created_at) values (4,1,10000,now
 
 
 
-insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('강아지 산책줄 나눔합니다.', '4', '2', '강아지 산책줄 나눔 중입니다.', '없습니다.', 1000, now(),'2024-12-15 23:00:00', 0);
+insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('강아지 산책줄 나눔합니다.', '4', '2', '강아지 산책줄 나눔 중입니다.', '/upload/category1.png', 1000, now(),'2024-12-15 23:00:00', 0);
 insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('책상 팝니다', '7', '2', '2년 썼고 생활기스 있어요.', '없습니다', 20000, now(),'2024-12-15 22:00:00', 0);
 
 insert into goods_tb(title, category_id, seller_id, content, img_url, starting_price, created_at, end_at, status) values ('냄비 팔아요', '8', '2', '산지 얼마 안돼서 새것 같아요.', '없습니다', 5000, now(),'2024-12-11 21:00:00', 0);
